466d7cd54732ce7b1379b633f3936a58f269735a
RodrigoBrandao <rod_brand@hotmail.com>
2020-08-08 19:09:38 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+$categorias = []; //variável categoria recebe um array vazio
+$categorias[] = 'infantil';
+$categorias[] = 'adolescente';
+$categorias[] = 'adulto';
+//$categorias[] = 'idoso';
+
+//print_r($categorias);
+
+$nome = 'Rodrigo';
+$idade = 18;
+
+//var_dump($nome);
+//var_dump($idade);
+
+if($idade >= 6 && $idade <= 12)
+{
+    for($i=0; $i <= count($categorias); $i++){
+        if($categorias[$i]=='infantil'){
+                echo "O nadador " .$nome. " compete na catedoria " .$categorias[$i];
+        } 
+    }
+} 
+else if ($idade >= 13 && $idade < 18) 
+{
+    for($i=0; $i <= count($categorias); $i++){
+        if($categorias[$i]=='adolescente'){
+                echo "O nadador " .$nome. " compete na catedoria " .$categorias[$i];
+        } 
+    }
+}
+else
+{
+    for($i=0; $i <= count($categorias); $i++){
+        if($categorias[$i]=='adulto'){
+                echo "O nadador " .$nome. " compete na catedoria " .$categorias[$i];
+        } 
+    }
+}
+
